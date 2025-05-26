bc345fa0176b55aad547017e7c6b8a62b951f446
Manda0708 <manda.andriambololona1@gmail.com>
2025-05-26 13:38:53 +0300

    Update table.sql

--- TODO/BDD/Module1/table.sql
+++ TODO/BDD/Module1/table.sql
@@ -9,7 +9,7 @@ CREATE TABLE departments(
 
 -- Table pour les types de contrat
 CREATE TABLE contract_types(
-   contract_type_id VARCHAR(50),),
+   contract_type_id VARCHAR(50),
    contract_type_name VARCHAR(50)  NOT NULL,
    PRIMARY KEY(contract_type_id)
 );
@@ -20,7 +20,7 @@ CREATE TABLE candidates(
    last_name VARCHAR(100)  NOT NULL,
    first_name VARCHAR(100)  NOT NULL,
    birth_date DATE,
-   address VARCHAR(255) ,
+   address VARCHAR(255),
    email VARCHAR(100)  NOT NULL,
    created_at DATETIME2 DEFAULT CURRENT_TIMESTAMP,
    PRIMARY KEY(candidate_id),
@@ -29,15 +29,15 @@ CREATE TABLE candidates(
 
 -- Table pour les types d'actions (pour la journalisation) (Create, Update, ... Delete, etc.)
 CREATE TABLE action_type(
-   action_type_id VARCHAR(50) ,
+   action_type_id VARCHAR(50),
    type VARCHAR(50)  NOT NULL,
    PRIMARY KEY(action_type_id)
 );
 
 -- Table pour les utilisateurs RH/Managers
 CREATE TABLE users(
-   user_id VARCHAR(50) ,
-   name VARCHAR(255) ,
+   user_id VARCHAR(50),
+   name VARCHAR(255),
    email VARCHAR(100)  NOT NULL,
    password VARCHAR(255)  NOT NULL,
    role VARCHAR(50)  NOT NULL,
@@ -50,13 +50,13 @@ CREATE TABLE users(
 CREATE TABLE job_offers(
    offer_id VARCHAR(50),
    title VARCHAR(200)  NOT NULL,
-   location VARCHAR(100) ,
+   location VARCHAR(100),
    deadline_date DATETIME2,
    description VARCHAR(max),
    required_skills VARCHAR(max),
    required_experience VARCHAR(max),
-   required_education VARCHAR(200) ,
-   required_languages VARCHAR(200) ,
+   required_education VARCHAR(200),
+   required_languages VARCHAR(200),
    status ENUM('DRAFT', 'PUBLISHED', 'CLOSED') DEFAULT 'DRAFT',
    created_at DATETIME2 DEFAULT CURRENT_TIMESTAMP,
    department_id INT,
@@ -68,7 +68,7 @@ CREATE TABLE job_offers(
 
 -- Table pour les candidatures
 CREATE TABLE applications(
-   application_id VARCHAR(50),,
+   application_id VARCHAR(50),
    application_date DATETIME2 DEFAULT CURRENT_TIMESTAMP,
    cv VARBINARY(50)  NOT NULL,
    motivation_letter VARBINARY(50)  NOT NULL,
@@ -96,12 +96,12 @@ CREATE TABLE cv_details(
 
 -- Table pour journaliser les actions (sécurité et conformité RGPD)
 CREATE TABLE action_logs(
-   log_id VARCHAR(50) ,
+   log_id VARCHAR(50),
    entity_type VARCHAR(50)  NOT NULL,
    action_description VARCHAR(max),
    action_at DATETIME2 NOT NULL,
-   action_type_id VARCHAR(50) ,
-   user_id VARCHAR(50) ,
+   action_type_id VARCHAR(50),
+   user_id VARCHAR(50),
    PRIMARY KEY(log_id),
    FOREIGN KEY(action_type_id) REFERENCES action_type(action_type_id),
    FOREIGN KEY(user_id) REFERENCES users(user_id)
@@ -110,10 +110,10 @@ CREATE TABLE action_logs(
 
 -- Table pour les commentaires RH sur les candidatures
 CREATE TABLE application_comments(
-   comment_id VARCHAR(50) ,
+   comment_id VARCHAR(50),
    comment_text VARCHAR(max),
    created_at DATETIME2 NOT NULL,
-   user_id VARCHAR(50) ,
+   user_id VARCHAR(50),
    application_id INT,
    PRIMARY KEY(comment_id),
    FOREIGN KEY(user_id) REFERENCES users(user_id),
