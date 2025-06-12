0253d4a238bdf96a4d77aba12eaec6deb38e4be7
Daxido junior <143500070+HenintsoaJunior@users.noreply.github.com>
2025-06-12 09:28:43 +0300

    CRUD DEPT,USR ...
    CRUD DEPT,USR ...

---
+++ BACKEND/rh-backend/src/MyApp.Api/Entities/users/User.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MyApp.Api.Entities.departments;
+
+namespace MyApp.Api.Entities.users
+{
+    [Table("users")]
+    public class User
+    {
+        [Key]
+        [Column("user_id")]
+        [MaxLength(50)]
+        public string UserId { get; set; } = null!;
+
+        [Column("name")]
+        [MaxLength(255)]
+        public string? Name { get; set; }
+
+        [Required]
+        [Column("email")]
+        [MaxLength(100)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [Column("password")]
+        [MaxLength(255)]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        [Column("role")]
+        [MaxLength(50)]
+        public string Role { get; set; } = null!;
+
+        [Required]
+        [Column("department_id")]
+        [MaxLength(50)]
+        [ForeignKey("Department")]
+        public string DepartmentId { get; set; } = null!;
+
+        public Department? Department { get; set; }
+    }
+}
