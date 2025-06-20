25a2a579cfbcce77a8f67e10ae7f022cbd5a30e0
Manda0708 <manda.andriambololona1@gmail.com>
2025-06-20 11:56:59 +0300

    commit

--- FRONTEND/rh-front/src/pages/Candidature.js
+++ FRONTEND/rh-front/src/pages/Candidature.js
@@ -2,8 +2,8 @@
 
 export default function Candidature() {
   return (
-      <div className="condidature-content">
-        <h2>New Condidature</h2>
+      <div className="candidature-content">
+        <h2>New Candidature</h2>
         <p>BIEN ZANDRY</p>
       </div>
   );
