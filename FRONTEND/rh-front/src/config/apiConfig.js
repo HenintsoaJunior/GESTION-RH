58641674b21989ac49f42638df0ae7f756c1f246
Daxido junior <143500070+HenintsoaJunior@users.noreply.github.com>
2025-08-08 10:32:32 +0300

    PUSHa
    PUSHa

--- FRONTEND/rh-front/src/config/apiConfig.js
+++ FRONTEND/rh-front/src/config/apiConfig.js
@@ -1,4 +1,5 @@
 // src/config/apiConfig.js
-export const BASE_URL = "http://localhost:5183";
+export const BASE_URL = "http://10.0.104.215:5183";
 
 export const BASE_URL_LDAP = "http://10.0.104.215:5094";
+
