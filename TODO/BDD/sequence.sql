3834273b754fc910be58868b72291701f75d2a6d
Manda0708 <manda.andriambololona1@gmail.com>
2025-09-09 09:01:49 +0300

    base note de frais

--- TODO/BDD/sequence.sql
+++ TODO/BDD/sequence.sql
@@ -373,6 +373,14 @@ IF EXISTS (SELECT * FROM sys.sequences WHERE name = 'seq_mission_budget_id')
     DROP SEQUENCE seq_mission_validation_id;
 GO
 
+IF EXISTS (SELECT * FROM sys.sequences WHERE name = 'seq_expense_report_type_id')
+    DROP SEQUENCE seq_mission_validation_id;
+GO
+
+IF EXISTS (SELECT * FROM sys.sequences WHERE name = 'seq_expense_report_id')
+    DROP SEQUENCE seq_mission_validation_id;
+GO
+
 IF EXISTS (SELECT * FROM sys.sequences WHERE name = 'seq_log_id')
     DROP SEQUENCE seq_log_id;
 GO
@@ -447,6 +455,24 @@ CREATE SEQUENCE seq_mission_validation_id
 GO
 
 CREATE SEQUENCE seq_mission_budget_id
+    AS INT
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1
+    NO CYCLE
+    CACHE 50;
+GO
+
+CREATE SEQUENCE seq_expense_report_type_id
+    AS INT
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1
+    NO CYCLE
+    CACHE 50;
+GO
+
+CREATE SEQUENCE seq_expense_report_id
     AS INT
     START WITH 1
     INCREMENT BY 1
